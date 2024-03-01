a4b8bbdb49982eb3f4b54f3894c62908c2c86e83
Ahmed Goda <ahmedgodaa2001@gmail.com>
2024-03-02 00:47:59 +0200

    v1.0.2
    - Add ID support 
    - Remove parent empty .md from Json

--- v1.0.2.py
+++ v1.0.2.py
@@ -8,14 +8,18 @@ def datetime_serializer(obj):
         return obj.isoformat()
     raise TypeError("Type not serializable")
 
-def extract_blog_data(markdown_path):
+def extract_blog_data(markdown_path, start_id):
     with open(markdown_path, 'r', encoding='utf-8') as file:
         content = frontmatter.load(file)
+
+    # Generate ID as an increasing integer starting from start_id
+    blog_id = start_id
 
     # Extract front matter keys and values
     front_matter = {key: content[key] for key in content.keys() if key != 'content'}
 
     return {
+        'id': blog_id,
         'front_matter': front_matter,
         'content': content.content
     }
@@ -28,10 +32,18 @@ def extract_all_blogs(directory, output_directory):
             if filename.endswith('.md'):
                 subdirectory_name = os.path.basename(root)
                 markdown_path = os.path.join(root, filename)
-                blog_data = extract_blog_data(markdown_path)
 
                 if subdirectory_name not in all_blogs:
                     all_blogs[subdirectory_name] = []
+                    counter = 0
+                else:
+                    counter = len(all_blogs[subdirectory_name])
+
+                blog_data = extract_blog_data(markdown_path, counter)
+
+                # Check if category is "Home" and skip conversion
+                if 'category' in blog_data['front_matter'] and blog_data['front_matter']['category'] == 'Home':
+                    continue
 
                 all_blogs[subdirectory_name].append(blog_data)
 
